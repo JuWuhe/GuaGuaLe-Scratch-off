5944840891ec45bb78dd0f8779343e36248b377d
JuWuhe <Insyent@foxmail.com>
2022-09-07 20:17:38 +0800

    Update Code

--- Assets/GuaGuaLe/EraseMask.cs
+++ Assets/GuaGuaLe/EraseMask.cs
@@ -1,98 +1,78 @@
 ﻿using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class EraseMask : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
+public class EraseMask : MonoBehaviour, IPointerDownHandler, IDragHandler
 {
     // 是否擦除了
     public bool isStartEraser;
     // 是否擦除结束了
     public bool isEndEraser;
-    // 开始事件
-    public Action eraserStartEvent;
-    // 结束事件
-    public Action eraserEndEvent;
     
-    public RawImage uiTex; // 上层图片
-    Texture2D tex;
-    Texture2D MyTex;
-    int mWidth;
-    int mHeight;
+    // 开始事件
+    private Action eraserStartEvent;
+    // 结束事件
+    private Action eraserEndEvent;
+    
+    // 被擦除的图片
+    [SerializeField]
+    private RawImage uiTex;
+    
+    private Texture2D tex;
+    private Texture2D myTex;
+    private int mWidth;
+    private int mHeight;
+    
     [Header("Brush Size")]
     public int brushSize = 50;
     [Header("Rate")]
     public int rate = 90;
     
     // 计算进度
-    float maxColorA;
-    float colorA;
+    private float maxColorA;
+    private float colorA;
 
-    void Awake()
+    private void Awake()
     {
         tex = (Texture2D)uiTex.mainTexture;
-        MyTex = new Texture2D(tex.width, tex.height, TextureFormat.ARGB32, false);
-        mWidth = MyTex.width;
-        mHeight = MyTex.height;
+        myTex = new Texture2D(tex.width, tex.height, TextureFormat.ARGB32, false);
+        mWidth = myTex.width;
+        mHeight = myTex.height;
+        myTex.SetPixels(tex.GetPixels());
+        myTex.Apply();
         
-        MyTex.SetPixels(tex.GetPixels());
-        MyTex.Apply();
+        uiTex.texture = myTex;
         
-        uiTex.texture = MyTex;
-        maxColorA = MyTex.GetPixels().Length;
-        // Debug.Log(maxColorA);
+        maxColorA = myTex.GetPixels().Length;
         colorA = 0;
+        
         isEndEraser = false;
         isStartEraser = false;
     }
 
-    bool twoPoints = false;
-    Vector2 lastPos;   // 最后一个点
-    Vector2 penultPos; // 倒数第二个点
-    float distance = 1f;
-    
-    #region 事件
+    #region MouseAction
     public void OnPointerDown(PointerEventData eventData)
     {
         if (isEndEraser) { return; }
-        penultPos = eventData.position;
-        CheckPoint(penultPos);
+        CheckPoint(eventData.position);
     }
 
     public void OnDrag(PointerEventData eventData)
     {
         if (isEndEraser) { return; }
-        if (twoPoints && Vector2.Distance(eventData.position, lastPos) > distance) // 如果两次记录的鼠标坐标距离大于一定的距离，开始记录鼠标的点
-        {
-            Vector2 pos = eventData.position;
-            float dis = Vector2.Distance(lastPos, pos);
-
-            CheckPoint(eventData.position);
-        }
-        else
-        {
-            twoPoints = true;
-            lastPos = eventData.position;
-        }
+        CheckPoint(eventData.position);
     }
-
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        if (isEndEraser) { return; }
-        //CheckPoint(eventData.position);
-        twoPoints = false;
-    }
+    
     #endregion
     
     /// <summary>
     /// 刮除点击周围
     /// </summary>
-    /// <param name="pScreenPos"></param>
-    void CheckPoint(Vector3 pScreenPos)
+    /// <param name="screenPos"></param>
+    private void CheckPoint(Vector3 screenPos)
     {
-        Vector3 worldPos = Camera.main.ScreenToWorldPoint(pScreenPos);
+        Vector3 worldPos = Camera.main.ScreenToWorldPoint(screenPos);
         Vector3 localPos = uiTex.gameObject.transform.InverseTransformPoint(worldPos);
         if (localPos.x > -mWidth / 2 && localPos.x < mWidth / 2 && localPos.y > -mHeight / 2 && localPos.y < mHeight / 2)
         {
@@ -112,12 +92,12 @@ public class EraseMask : MonoBehaviour, IPointerDownHandler, IDragHandler, IPoin
                     // 获取像素
                     // 纹理坐标从左下角开始
                     // Texture coordinates start at lower left corner.
-                    Color col = MyTex.GetPixel(i + (int)mWidth / 2, j + (int)mHeight / 2);
+                    Color col = myTex.GetPixel(i + (int)mWidth / 2, j + (int)mHeight / 2);
                     if (col.a != 0f)
                     {
                         col.a = 0.0f;
                         colorA++;
-                        MyTex.SetPixel(i + (int)mWidth / 2, j + (int)mHeight / 2, col);
+                        myTex.SetPixel(i + (int)mWidth / 2, j + (int)mHeight / 2, col);
                     }
                 }
             }
@@ -125,20 +105,26 @@ public class EraseMask : MonoBehaviour, IPointerDownHandler, IDragHandler, IPoin
             if (!isStartEraser)
             {
                 isStartEraser = true;
-                InvokeRepeating("getTransparentPercent", 0f, 0.2f);
-                if (eraserStartEvent != null)
-                    eraserStartEvent.Invoke();
+                InvokeRepeating(nameof(GetTransparentPercent), 0f, 0.2f);
+                eraserStartEvent += () =>
+                {
+                    Debug.Log("开始擦除");
+                };
+                eraserStartEvent?.Invoke();
             }
-            MyTex.Apply();
+            myTex.Apply();
         }
     }
     
-    double fate;
+    /// <summary>
+    /// 当前擦除进度
+    /// </summary>
+    private double fate;
     /// <summary> 
-    /// 检测当前刮刮卡 进度
+    /// 检测当前擦除进度
     /// </summary>
     /// <returns></returns>
-    public void getTransparentPercent()
+    private void GetTransparentPercent()
     {
         if (isEndEraser) { return; }
         fate = colorA / maxColorA * 100;
@@ -147,11 +133,13 @@ public class EraseMask : MonoBehaviour, IPointerDownHandler, IDragHandler, IPoin
         if (fate >= rate)
         {
             isEndEraser = true;
-            CancelInvoke("getTransparentPercent");
-            uiTex.gameObject.SetActive(false);
+            CancelInvoke(nameof(GetTransparentPercent));
             //触发结束事件
-            if (eraserEndEvent != null)
-                eraserEndEvent.Invoke();
+            eraserEndEvent += () =>
+            {
+                uiTex.gameObject.SetActive(false);
+            };
+            eraserEndEvent?.Invoke();
         }
     }
 }
